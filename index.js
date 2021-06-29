f262770a1352a35f5bd698d1544ced7a90c5a792
NayanSidhu <nsidh068@uottawa.ca>
2021-06-28 22:25:40 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/* 
+Author: Nayandeep Sidhu 
+Date Created: June 28th, 2021
+Project Description: This project uses node and npn to generate a professional README generator. The program will ask the user for a series of questions that will allow the user to generate a README. Some questions will be mandatory to respond to while others may be skipped. */ 
+
+
+//To import inquirer onto our JS file
+const inquirer = require('inquirer'); 
